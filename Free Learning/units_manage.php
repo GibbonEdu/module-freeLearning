a661afd0f636de607b9c18d94b9f8e73a5e84a84
Karim <kchong@ichk.edu.hk>
2019-04-19 11:01:29 +0800

    Refactored Manage Units Breadcrumbs

--- Free Learning/units_manage.php
+++ Free Learning/units_manage.php
@@ -32,9 +32,11 @@ if (isActionAccessible($guid, $connection2, '/modules/Free Learning/units_manage
         echo __($guid, 'The highest grouped action cannot be determined.');
         echo '</div>';
     } else {
-        echo "<div class='trail'>";
-        echo "<div class='trailHead'><a href='".$_SESSION[$guid]['absoluteURL']."'>".__($guid, 'Home')."</a> > <a href='".$_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/'.getModuleName($_GET['q']).'/'.getModuleEntry($_GET['q'], $connection2, $guid)."'>".__($guid, getModuleName($_GET['q']), 'Free Learning')."</a> > </div><div class='trailEnd'>".__($guid, 'Manage Units', 'Free Learning').'</div>';
-        echo '</div>';
+    	//Breadcrumbb
+    	$page->breadcrumbs
+    	->add(__('Manage Units'));
+    	
+
 
         if (isset($_GET['return'])) {
             returnProcess($guid, $_GET['return'], null, null);
