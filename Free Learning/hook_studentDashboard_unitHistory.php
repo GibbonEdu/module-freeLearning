c27ce2f98b36e08bd90b94600337434950278759
Sandra Kuipers <sandra@skuipers.com>
2020-10-13 13:06:34 +0800

    v5.10.03 post-merge fix

--- Free Learning/hook_studentDashboard_unitHistory.php
+++ Free Learning/hook_studentDashboard_unitHistory.php
@@ -17,6 +17,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+global $gibbon;
+
 $returnInt = null;
 
 //Only include module include if it is not already included (which it may be been on the index page)
--- Free Learning/hook_studentProfile_unitHistory.php
+++ Free Learning/hook_studentProfile_unitHistory.php
@@ -17,6 +17,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+global $gibbon;
+
 //Module includes
 require_once './modules/Free Learning/moduleFunctions.php';
 
