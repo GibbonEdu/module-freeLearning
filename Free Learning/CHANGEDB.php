5076d75ecff19968cdc63bc828828d5476cf5169
Ross Parker <rossdotparker@gmail.com>
2021-02-02 19:24:58 +0800

    v5.15.01 Take 2

--- Free Learning/CHANGEDB.php
+++ Free Learning/CHANGEDB.php
@@ -1396,13 +1396,6 @@ $sql[$count][1] = "
 INSERT INTO `gibbonSetting` (`gibbonSettingID` ,`scope` ,`name` ,`nameDisplay` ,`description` ,`value`) VALUES (NULL , 'Free Learning', 'certificatesAvailable', 'Certificates Available', 'Should certificates be made available on unit completion?', 'Y');end
 ";
 
-//v5.14.05
-++$count;
-$sql[$count][0] = '5.14.05';
-$sql[$count][1] = "
-ALTER TABLE `freeLearningBadge` CHANGE `unitsCompleteTotal` `unitsCompleteTotal` int(3) NULL DEFAULT NULL, CHANGE `unitsCompleteThisYear` `unitsCompleteThisYear` int(3) NULL DEFAULT NULL, CHANGE `unitsCompleteDepartmentCount` `unitsCompleteDepartmentCount` int(3) NULL DEFAULT NULL, CHANGE `unitsCompleteIndividual` `unitsCompleteIndividual` int(3) NULL DEFAULT NULL, CHANGE `unitsCompleteGroup` `unitsCompleteGroup` int(3) NULL DEFAULT NULL;end
-";
-
 //v5.15.00
 ++$count;
 $sql[$count][0] = '5.15.00';
@@ -1414,4 +1407,5 @@ UPDATE gibbonDiscussion JOIN freeLearningUnitStudent ON (gibbonDiscussion.foreig
 ++$count;
 $sql[$count][0] = '5.15.01';
 $sql[$count][1] = "
+ALTER TABLE `freeLearningBadge` CHANGE `unitsCompleteTotal` `unitsCompleteTotal` int(3) NULL DEFAULT NULL, CHANGE `unitsCompleteThisYear` `unitsCompleteThisYear` int(3) NULL DEFAULT NULL, CHANGE `unitsCompleteDepartmentCount` `unitsCompleteDepartmentCount` int(3) NULL DEFAULT NULL, CHANGE `unitsCompleteIndividual` `unitsCompleteIndividual` int(3) NULL DEFAULT NULL, CHANGE `unitsCompleteGroup` `unitsCompleteGroup` int(3) NULL DEFAULT NULL;end
 ";
