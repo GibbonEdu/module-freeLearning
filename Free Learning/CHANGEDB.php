81f940506acbdab38a8f8b614f40c32599e56dc9
Ross Parker <rossdotparker@gmail.com>
2021-02-18 22:17:57 +0800

    v5.14.09

--- Free Learning/CHANGEDB.php
+++ Free Learning/CHANGEDB.php
@@ -1420,3 +1420,9 @@ $sql[$count][1] = "
 $sql[$count][0] = '5.14.08';
 $sql[$count][1] = "
 ";
+
+//v5.14.09
+++$count;
+$sql[$count][0] = '5.14.09';
+$sql[$count][1] = "
+";
--- Free Learning/version.php
+++ Free Learning/version.php
@@ -20,4 +20,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /**
  * Sets version information.
  */
-$moduleVersion = '5.14.08';
+$moduleVersion = '5.14.09';
