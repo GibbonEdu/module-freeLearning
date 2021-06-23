d4b675fc958df328dfc42cfd3c81dbff77488c23
Sandra Kuipers <sandra@skuipers.com>
2021-06-23 10:41:31 +0800

    Update badges_manage.php

--- Free Learning/badges_manage.php
+++ Free Learning/badges_manage.php
@@ -96,7 +96,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Free Learning/badges_manag
            ->displayLabel();
 
         $table->addColumn('logo', __('Logo'))
-            ->format(function ($values) use ($gibbon) {
+            ->format(function ($values) use ($session) {
                 if ($values['logo'] == null) {
                     return "<img style='margin: 5px; height: 125px; width: 125px' class='user' src='".$session->get('absoluteURL').'/themes/'.$session->get('gibbonThemeName')."/img/anonymous_125.jpg'/><br/>";
                 } else {
