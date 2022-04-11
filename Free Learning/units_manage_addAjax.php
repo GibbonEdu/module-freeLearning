000ae098e7754c3e7c8dcb1720a54a47ff12b825
Ray Clark <raynichc@users.noreply.github.com>
2022-04-11 16:43:46 +1000

    Implemented copy unit block ajax backend

---
+++ Free Learning/units_manage_addAjax.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Gibbon, Flexible & Open School System
+Copyright (C) 2010, Ross Parker
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+use Gibbon\Module\FreeLearning\Domain\UnitBlockGateway;
+
+$_POST['address'] = '/modules/Free Learning/units_manage_add.php';
+
+require_once '../../gibbon.php';
+
+if (empty($gibbon->session->get('gibbonPersonID')) || empty($gibbon->session->get('gibbonRoleIDPrimary'))
+    || !isActionAccessible($guid, $connection2, '/modules/Free Learning/units_manage_add.php')) {
+    die(__('Your request failed because you do not have access to this action.'));
+} else {
+    $freeLearningUnitBlockID = $_POST['freeLearningUnitBlockID'] ?? null;
+
+    if ($freeLearningUnitBlockID == null) {
+        echo -1;
+        die();
+    }
+
+
+    $unitBlockGateway = $container->get(UnitBlockGateway::class);
+
+    echo json_encode($unitBlockGateway->getByID($freeLearningUnitBlockID));
+}
+
+?>
