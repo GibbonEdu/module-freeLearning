23629bcb431fb4725f3c1ed911b96863e267edb9
Ali Alam <aali@ichk.edu.hk>
2025-03-18 11:07:59 +0800

    Bug: Fixed the issue of correctly formatting URL links to remove any .,;: at the end of an URL Link.

--- Free Learning/manifest.php
+++ Free Learning/manifest.php
@@ -27,7 +27,7 @@ $description = "Free Learning is a module which enables a student-focused and st
 $entryURL = 'units_browse.php';
 $type = 'Additional';
 $category = 'Learn';
-$version = '5.25.07';
+$version = '5.25.08';
 $author = "Gibbon Foundation";
 $url = "https://gibbonedu.org";
 
