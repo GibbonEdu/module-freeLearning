ac3044382bdfff315c68495ae3c8ea258527ccb1
Ali Alam <aalam@ichk.edu.hk>
2025-06-20 09:28:51 +0800

    System: Replaced $gibbon->session to $session since "$gibbon->session" has been deprecated (#50)
    Co-authored-by: Ali Alam <aali@ichk.edu.hk>

--- Free Learning/manifest.php
+++ Free Learning/manifest.php
@@ -27,7 +27,7 @@ $description = "Free Learning is a module which enables a student-focused and st
 $entryURL = 'units_browse.php';
 $type = 'Additional';
 $category = 'Learn';
-$version = '5.29.00';
+$version = '5.29.01';
 $author = "Gibbon Foundation";
 $url = "https://gibbonedu.org";
 
