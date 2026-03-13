db86cc28174c50893edfc1304ea7de1861fbc055
Ali Alam <aalam@ichk.edu.hk>
2026-03-13 14:14:06 +0800

    Free Learning: Added comprehensive file upload tracking system to monitor and manage all file uploads across the system

--- Free Learning/version.php
+++ Free Learning/version.php
@@ -22,5 +22,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /**
  * Sets version information.
  */
-$moduleVersion = '5.29.13';
+$moduleVersion = '5.30.00';
 $coreVersion = '28.0.00';
